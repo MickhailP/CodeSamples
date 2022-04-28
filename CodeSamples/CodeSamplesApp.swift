440dc77e18e49699fa9c733948286c9b5b302ebd
MickhailP <mich_p@inbox.ru>
2022-04-28 07:19:45 +0300

    Initial Commit

---
+++ CodeSamples/CodeSamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeSamplesApp.swift
+//  CodeSamples
+//
+//  Created by Миша Перевозчиков on 28.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct CodeSamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
